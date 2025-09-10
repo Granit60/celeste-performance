abe32ead9431bb5635d738dfb4b08ec70290a099
Snekye <dimitrigranit22@gmail.com>
2025-09-10 10:58:53 +0200

    Randomised lisa

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { RandomisedIcon as Lisa } from './components/RandomisedIcon';
 
 export default function App() {
   return (
     <div className="layout">
+      <Lisa></Lisa>
       <Header />
       <main>
         <Outlet />
---
+++ src/components/RandomisedIcon.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+export function RandomisedIcon() {
+    const icons = import.meta.glob("/src/img/*.webp", { eager: true });
+    const iconList = Object.values(icons).map(mod => mod.default);
+    const randomIcon = iconList[Math.floor(Math.random() * iconList.length)];
+
+    console.log(randomIcon);
+
+    return (
+        <Helmet>
+            <link rel="icon" type="image/webp" href={ randomIcon }></link>
+        </Helmet>
+    )
+}
